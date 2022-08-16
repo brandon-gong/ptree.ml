dad250d1f5871ccfadc860fb31608c274400424b
brandon-gong <brandon.gong@brandongong.org>
2022-08-16 10:41:22 -0500

    add docs

--- lib/parsing.mli
+++
@@ -1,6 +0,0 @@
-val peditor_of_ini : string -> Ptreedata.peditor
-val peditor_of_json : string -> Ptreedata.peditor
-val peditor_of_xml : string -> Ptreedata.peditor
-val peditor_of_ini_opt : string -> Ptreedata.peditor option
-val peditor_of_json_opt : string -> Ptreedata.peditor option
-val peditor_of_xml_opt : string -> Ptreedata.peditor option
--- lib/pprint.mli
+++
@@ -1,9 +0,0 @@
-open Ptreedata
-
-val validate_ini_key : string -> string
-val ini_of_peditor : peditor -> string
-val json_of_peditor_tab : string -> peditor -> string
-val json_of_peditor : peditor -> string
-val validate_xml_key : string -> string
-val xml_of_peditor_tab : string -> string -> peditor -> string
-val xml_of_peditor : string -> peditor -> string
